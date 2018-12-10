d2234d0b17ddcb5c78f89011f65ddff2a00656ff
Malte Hildebrand <malte.hildebrand@gmail.com>
2018-12-10 11:25:22 +0100

    add datawrapp-bubble.css.d.ts

---
+++ src/ui/datawrapper-bubble/datawrapper-bubble.css.d.ts
@@ -0,0 +1,7 @@
+export const bubblePollButtons: string;
+export const bubbleTextPadding: string;
+export const bubbleText: string;
+export const bubblePollButtonsContainer: string;
+export const isLoadingSpinnerContainer: string;
+export const isLoadingSpinner: string;
+export const hiddenChatBubble: string;
